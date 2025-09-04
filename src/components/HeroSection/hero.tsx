a77d1b5367873ff6cb61071ccfae97d1a079d9c5
julietoge <julietogechi27@gmail.com>
2025-09-04 18:51:37 +0100

    update hero section

--- src/components/HeroSection/hero.tsx
+++ src/components/HeroSection/hero.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
       <div className="bg-[url('/hero-section/background-1.png')] bg-cover bg-center bg-no-repeat mx-auto px-4 sm:px-8 lg:px-16 xl:px-20 2xl:px-24">
         <div className="grid grid-cols-1 md:grid-cols-2 min-h-[600px]">
           {/* Text Content Section */}
-          <div className="space-y-5 flex flex-col justify-center items-center text-center md:text-left py-20 px-10 md:pr-30 md:py-0 md:px-0 md:items-start">
+          <div className="space-y-5 flex flex-col justify-center items-center text-center md:text-left py-20 px-10 md:pr-30 md:py-0 md:px-0 md:items-start order-2 md:order-1">
             <motion.h1
               variants={SlideUp(0.2)}
               initial="initial"
@@ -36,7 +36,7 @@ const HeroSection = () => {
             </motion.button>
           </div>
           {/* Hero image Section */}
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center order-1 md:order-2">
             <motion.img initial={{opacity: 0, x: 100}}
             transition={{duration: 0.5, delay: 0.5,}}
               animate={{opacity: 1, x: 0}}
