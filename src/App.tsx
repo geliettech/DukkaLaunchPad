cb1ca7875daf3558f0f96f0cf5ebff49e65f24a9
julietoge <julietogechi27@gmail.com>
2025-09-08 01:22:38 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PackageDetails from "./pages/packageDetails";
 import { Routes, Route } from "react-router";
 import Header from "./components/header/header.tsx";
 import Footer from "./components/footer/footer.tsx";
+import MultiStepForm from "./pages/forms/multiform.tsx";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<LandingPage />} />
         {/* <Route path="/Packages" element={<Packages />} /> */}
         <Route path="/Packages/:title" element={<PackageDetails />} />
+        <Route path="/multiform"  element={<MultiStepForm />} />
       </Routes>
       <Footer />
     </main>
